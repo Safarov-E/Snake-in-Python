b0c85dbdd9b8d0921b8df871de308d3a4b49a2f6
Safarov <elkhan.safarov10@yandex.com>
2020-12-21 13:29:25 +0500

    Начало Игры

--- snake-in-python.py
+++ snake-in-python.py
@@ -15,15 +15,31 @@ y = [0] * int(counBodyW)
 class Snake(Canvas):
     headImage = False
     head = False
+    body = False
+    apple = False
+    delay = 0
+    direction = 'Right'
+    loss = False
+
     def __init__(self):
         Canvas.__init__(self, width=WIDTH, height=HEIGHT, background="black")
         self.focus_get()
         self.bind_all("<Key>", self.onKeyPressed)
         self.loadResources()
+        self.beginplay()
 
     def loadResources(self):
         self.headImage = Image.open('./images/head.png')
-        self.head = ImageTk.PhotoImage(self.headImage.resize((BODYSIZE, BODYSIZE), Image.ANTIALLAS))
+        self.head = ImageTk.PhotoImage(self.headImage.resize((BODYSIZE, BODYSIZE), Image.ANTIALIAS))
+        self.body = ImageTk.PhotoImage(Image.open('./images/body.png').resize((BODYSIZE, BODYSIZE), Image.ANTIALIAS))
+        self.apple = ImageTk.PhotoImage(Image.open('./images/apple.png').resize((BODYSIZE, BODYSIZE), Image.ANTIALIAS))
+
+    def beginplay(self):
+        self.delay = STARTDELAY
+        self.direction = "Right"
+        self.loss = False
+
+        self.delete(ALL)
 
     def onKeyPressed(self, event):
         pass
